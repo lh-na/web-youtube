33433f2acac21384fdb5ebcf4f53dd6c69719814
lh-na <lhna1110@naver.com>
2025-03-12 14:45:08 +0900

    페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Today from './pages/Today'
+import Music from './pages/Music'
+import Movie from './pages/Movie'
+import Book from './pages/Book'
+import Anime from './pages/Anime'
+import Port from './pages/Port'
+import Youtube from './pages/Youtube'
+import Channel from './pages/Youtube'
+import Video from './pages/Video'
+import Search from './pages/Search'
+import Not from './pages/Not'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/today' element={<Today/>}/>
+        <Route path='/music' element={<Music/>}/>
+        <Route path='/movie' element={<Movie/>}/>
+        <Route path='/book' element={<Book/>}/>
+        <Route path='/anime' element={<Anime/>}/>
+        <Route path='/port' element={<Port/>}/>
+        <Route path='/yotube' element={<Youtube/>}/>
+        <Route path='/channel/:channelID' element={<Channel/>}/>
+        <Route path='/video/:vedioID' element={<Video/>}/>
+        <Route path='/search/:searchID' element={<Search/>}/>
+        <Route path='/*' element={<Not/>}/>  
+      </Routes>
+    </BrowserRouter>
   )
 }
 
