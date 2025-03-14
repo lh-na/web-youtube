287bc0bb96c4f474c1708edd30b1ee741bec64d4
lh-na <lhna1110@naver.com>
2025-03-14 10:42:45 +0900

    사스 미디어 작업

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import { Link } from 'react-router-dom'
+import { todayText } from '../data/Today';
 
 const Today = () => {
+  
   return (
-    <Main title="유튜브 추천" description="유튜브 추천 페이지입니다.">
-        Today
+    <Main 
+        title="유튜브 추천" 
+        description="유튜브 추천 페이지입니다.">
+        
+        <section id='todayPage'>
+          <h2>🎀오늘의 추천 영상입니다🎀</h2>
+          {todayText.map((today, key)=>(
+                    <div className='today__inner' key={key}>
+                        <div className='today__thumb play__icon'>
+                            <Link to={today.page}>
+                            <img src={today.img} alt={today.title}/>
+                            </Link>
+                        </div>
+                        <div className='today__text'>
+                            <span className='today'>today!</span>
+                            <h3 className='title'>
+                                <Link to={today.page}>
+                                {today.title}
+                                </Link>
+                                </h3>
+                            <p className='desc'>{today.desc}</p>
+                            <div className='info'>
+                                <span className='author'>
+                                    <Link to={`/channel/${today.channelId}`}>
+                                    {today.author}
+                                    </Link>
+                                    </span>
+                                <span className='date'>{today.date}</span>
+                            </div>
+                        </div>
+                    </div>
+          ))}
+        </section>
+        
+
+        
+                
     </Main>
   )
 }
